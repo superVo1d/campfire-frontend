277ced6a6c19c440d7804d892562acfbd74460f4
Grigory Mylnikov <supervoid18@gmail.com>
2023-12-25 19:55:00 +0300

    fix(settings): Проблемы с оверфлоу

--- src/app/modules/home/pages/home/home.page.ts
+++ src/app/modules/home/pages/home/home.page.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  styleUrls: ['./home.page.scss']
 })
-export class HomePageComponent {
-  //
-}
+export class HomePageComponent {}
